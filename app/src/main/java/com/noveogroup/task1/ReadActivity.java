8bf0d18b1ef4d8d2badf858e6566a5c0a7dc8eb0
KhegayIV <i.v.khegay@gmail.com>
2014-07-21 15:29:24 +0700

    Create ReadActivity and layouts
    Create ReadActivity and portrait and landscape layouts

---
+++ app/src/main/java/com/noveogroup/task1/ReadActivity.java
@@ -0,0 +1,20 @@
+package com.noveogroup.task1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.noveogroup.task1.R;
+
+public class ReadActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_read);
+	}
+
+	public void sendInfo(View view) {
+
+	}
+}
