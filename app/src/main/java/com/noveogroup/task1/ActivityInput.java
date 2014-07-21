6f6a66b998e57870a9229108a81f5a0c7f4dd614
Sergey Bragin <sergeybragin@rocketmail.com>
2014-07-21 18:15:25 +0700

    Implement ActivityInput
    Implement ActivityInput.

---
+++ app/src/main/java/com/noveogroup/task1/ActivityInput.java
@@ -0,0 +1,18 @@
+package com.noveogroup.task1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ActivityInput extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input);
+    }
+
+    public void sendInformation(View view) {
+
+    }
+}
