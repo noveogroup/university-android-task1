e3e94c8a73a28c4f6b79132c9015c7ffd080ce3e
tehnik819 <tehnik.819@gmail.com>
2014-07-23 18:31:03 +0700

    Some fixes

--- app/src/main/java/com/noveogroup/task1/Input.java
+++ app/src/main/java/com/noveogroup/task1/Input.java
@@ -1,13 +1,16 @@
 package com.noveogroup.task1;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import java.util.Calendar;
+import java.util.Date;
 
 
 public class Input extends Activity {
@@ -16,6 +19,7 @@ public class Input extends Activity {
     private EditText lastName;
     private DatePicker birthday;
 
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +28,8 @@ public class Input extends Activity {
         firstName = (EditText) findViewById(R.id.editFirstName);
         lastName = (EditText) findViewById(R.id.editLastName);
         birthday = (DatePicker) findViewById(R.id.dateBirthday);
+        Date d = new Date();
+        birthday.setMaxDate(d.getTime());
 
         showInfo.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,10 +47,12 @@ public class Input extends Activity {
     private int calcAge(final DatePicker d) {
         Calendar c = Calendar.getInstance();
         int age = c.get(Calendar.YEAR) - d.getYear();
-        if(d.getMonth() < c.get(Calendar.MONTH))
+        if(d.getMonth() < c.get(Calendar.MONTH)) {
             age--;
-        else if(d.getMonth() == c.get(Calendar.MONTH) && d.getDayOfMonth() > c.get(Calendar.DAY_OF_MONTH))
+        }
+        else if(d.getMonth() == c.get(Calendar.MONTH) && d.getDayOfMonth() > c.get(Calendar.DAY_OF_MONTH)) {
             age--;
+        }
         return age;
     }
 }
