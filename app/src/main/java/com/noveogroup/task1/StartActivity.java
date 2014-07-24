2fe0318e4f09720b1ac4ba63ecc8fe625d7c5c3e
KhegayIV <i.v.khegay@gmail.com>
2014-07-24 19:13:18 +0700

    Create fragments
    Create fragments and move activity contents to fragments

---
+++ app/src/main/java/com/noveogroup/task1/StartActivity.java
@@ -0,0 +1,16 @@
+package com.noveogroup.task1;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+import com.noveogroup.task1.R;
+
+public class StartActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_start);
+	}
+
+}
